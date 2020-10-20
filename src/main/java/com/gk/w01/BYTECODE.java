0655a46aa5c4250b13e49a522ed98871335d9252
ChristineLei <christine2food@gmail.com>
2020-10-20 11:07:22 +0800

    initial hello msg

--- src/main/java/com/gk/w01/BYTECODE.java
+++ src/main/java/com/gk/w01/BYTECODE.java
@@ -2,6 +2,6 @@ package com.gk.w01;
 
 public class BYTECODE {
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("hello bytecode");
     }
 }
